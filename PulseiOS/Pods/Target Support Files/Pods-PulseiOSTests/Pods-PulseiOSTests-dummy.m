8a642f7fb5214df63a9d11d74592b9ba87d63924
Max <sync95@gmail.com>
2015-11-07 13:51:27 -0500

    Added Pulse For iOS Target

---
+++ PulseiOS/Pods/Target Support Files/Pods-PulseiOSTests/Pods-PulseiOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PulseiOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_PulseiOSTests
+@end
---
+++ PulseiOS/Pods/Target Support Files/Pods-PulseiOSTests/Pods-PulseiOSTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PulseiOSTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PulseiOSTestsVersionString[];
+
