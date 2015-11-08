58193206faec8cd3a1ad9e7d7b79c73934a52124
Varun Patel <varun@verve3.io>
2015-11-08 05:57:49 -0500

    Update StoryBoard for to Expand Answer Text Field

--- PulseiOS/PulseiOS/FullQuestionViewController.swift
+++ PulseiOS/PulseiOS/FullQuestionViewController.swift
@@ -16,24 +16,5 @@ class FullQuestionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fullQuestionText.text = fullQuestion!
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
