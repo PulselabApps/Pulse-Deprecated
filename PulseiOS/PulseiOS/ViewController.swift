30cd2f233e125d79749a9e1baa87aa3054ff703b
Varun Patel <varun@verve3.io>
2015-11-07 19:43:33 -0500

    Add SubmitButton

--- PulseiOS/PulseiOS/ViewController.swift
+++ PulseiOS/PulseiOS/ViewController.swift
@@ -12,7 +12,9 @@ import Charts
 class ViewController: UIViewController {
 
     @IBOutlet weak var progressPieChart: UIView!
-
+    @IBOutlet weak var rank: UILabel!
+    @IBOutlet weak var points: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,7 +26,6 @@ class ViewController: UIViewController {
         
     }
     
-    
     func drawPieChart(correct: Double, incorrect: Double, pieChart: PieChartView) {
         var chartDataSetEntries = [ChartDataEntry]()
         chartDataSetEntries.append(ChartDataEntry(value: correct, xIndex: 0))
@@ -34,9 +35,11 @@ class ViewController: UIViewController {
         
         
         chartDataSet.colors = ChartColorTemplates.liberty()
-        chartDataSet.colors = [UIColor(), UIColor.magentaColor()]
-        // chartDataSet.colorAt(0) = UIColor.redColor()
         
+        let correctColor = UIColor(red: 91.0 / 255, green: 201.0 / 255, blue: 139.0 / 255, alpha: 1.0)
+        let incorrectColor = UIColor(red: 201.0 / 255, green: 91.0 / 255, blue: 104.0 / 255, alpha: 1.0)
+        
+        chartDataSet.colors = [correctColor, incorrectColor]
         
         let chartData = PieChartData(xVals: ["✔", "✕"], dataSet: chartDataSet)
         pieChart.data = chartData
@@ -48,6 +51,14 @@ class ViewController: UIViewController {
         pieChart.frame = CGRect(x: -40.0, y: -40.0, width: progressPieChart.frame.width * 2.0, height: progressPieChart.frame.height * 2.0)
         // pieChart.center = progressPieChart.center
         progressPieChart.addSubview(pieChart)
+    }
+    
+    func calculatePoints(){
+        
+    }
+    
+    func calculateRank() {
+        
     }
 
 }
