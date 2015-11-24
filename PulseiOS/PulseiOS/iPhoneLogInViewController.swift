4e04c9a0e1e38ec5b5e0cc6aba84ed154fb6ef56
Varun Patel <varun@verve3.io>
2015-11-24 02:45:13 -0500

    -Add Keyboard Watchers -userName Constraint Outlet -LogInButton Constrain Outlet
    -Need to do:
    --Implement calculating the constraints when keyboard appears/disappears

--- PulseiOS/PulseiOS/iPhoneLogInViewController.swift
+++ PulseiOS/PulseiOS/iPhoneLogInViewController.swift
@@ -10,7 +10,42 @@ import UIKit
 
 class iPhoneLogInViewController: DeviceLogInViewController {
     
+    @IBOutlet weak var userNameCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomConstraintLogInButton: NSLayoutConstraint!
+    var keyboardIsVisible = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
     }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if !keyboardIsVisible{
+            if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                self.keyboardIsVisible = true
+                if ((self.loginButton.center.y) >  (self.view.frame.height - keyboardSize.height)) {
+                    
+                    UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                           // MARK: adjust bottomConstraintLogInButton here
+                        }, completion: nil)
+                }
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if keyboardIsVisible {
+            if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                if ((self.loginButton.center.y) >  (self.view.frame.height - keyboardSize.height)) {
+                    UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+                          // MARK: adjust bottomConstraintLogInButton here
+                        }, completion: nil)
+                }
+            }
+        }
+        keyboardIsVisible = false
+    }
+
 }
