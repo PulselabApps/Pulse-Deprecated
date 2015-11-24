004ac07afcda61bb547c7fb49247d67ac4691d79
Max <sync95@gmail.com>
2015-11-24 11:51:20 -0500

    Added BuildScript

---
+++ build.sh
@@ -0,0 +1,14 @@
+# Clean repository, DerivedData folder and simulator
+git reset --hard
+git clean -fd
+rm -rf ~/Library/Developer/Xcode/DerivedData/PulseiOS*
+xcrun simctl erase all
+# Unit tests
+xctool \
+-scheme PulseiOS \
+-workspace /PulseiOS/PulseiOS.xcworkspace \
+-destination "platform=iOS Simulator,name=iPhone 6,OS=8.3" \
+-sdk iphonesimulator \
+-reporter junit:test-results/TEST-UnitTests.xml \
+-reporter pretty \
+clean test
